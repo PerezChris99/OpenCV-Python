d06b619c097e8d1df9bdc79c2da5a759f152baed
perezchris3599 <kweeziperez712@gmail.com>
2023-02-22 02:26:31 +0300

    added haarcascades

--- recsec/main.py
+++ recsec/main.py
@@ -4,8 +4,13 @@ import datetime
 
 cap = cv2.VideoCapture(0)
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
 while True:
     _, frame = cap.read()
+
+    
 
     cv2.imshow("Camera", frame)
 
