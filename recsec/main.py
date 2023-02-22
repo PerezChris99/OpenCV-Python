7616ca71f09cd7d4b1d0d0a3e5a152f807941bdd
perezchris3599 <kweeziperez712@gmail.com>
2023-02-22 16:37:15 +0300

    recsec complete

--- recsec/main.py
+++ recsec/main.py
@@ -7,15 +7,52 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
+detection = False
+detection_stopped_time = None
+timer_started = False
+SECONDS_TO_RECORD_AFTER_DETECTION = 5
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
 while True:
     _, frame = cap.read()
 
-    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(gray, 1.3, 5)
+    body = body_cascade.detectMultiScale(gray, 1.3, 5)
+
+    if len(face) + len(body) > 0:
+        if detection:
+            timer_started = False
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20.0, frame_size)
+            print("Started Recording Footage")
+
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                timer_started = False
+                out.release()
+                print("Stopped Recording Footage")
+        else:
+            timer_started = True
+            detection_stopped_time = time.time()
+
+    if detection:
+        out.write(frame)
+
+    #for (x, y, w, h) in face:
+        #cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
     cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+out.release()
 cap.release()
 cv2.destroyAllWindows()
